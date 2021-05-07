16277281fe59dfd4ce3860eb3b91604834b7bd13
Julia <69167984+Julia-Kalyukh@users.noreply.github.com>
2021-05-07 14:04:30 +0300

    commit

---
+++ deposit_calculator.js
@@ -0,0 +1,11 @@
+
+//Калькулятор вклада
+
+var money = parseInt(prompt('Сумма вклада: '));
+var interests = parseInt(prompt('Процентная ставка: '));
+var years = parseInt(prompt('Срок вклада: '));
+
+for (var n = 1; n <= years; n++) {
+    money = money + money * interests / 100;
+    alert('Прошло лет: ' + n + ' сумма на счету: ' + Math.floor(money));
+}
