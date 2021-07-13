8e72e308ac11c4f27c0d51f9b3e28b49112442da
Julia <69167984+Julia-Kalyukh@users.noreply.github.com>
2021-07-14 00:32:13 +0300

    step_11
    Использование классов для создания карточек меню

--- page_food/js/script.js
+++ page_food/js/script.js
@@ -1,8 +1,3 @@
-// Задачи:
-    // а) Вызов модального окна через определенное время;
-    // б) Если пользователь долистал до конца страницы - вывести модальное окно.
-
-
 window.addEventListener('DOMContentLoaded', function() {
 
 // Tabs
@@ -104,11 +99,9 @@ window.addEventListener('DOMContentLoaded', function() {
           modal = document.querySelector('.modal'),
           modalCloseBtn = document.querySelector('[data-close]');
 
-    // 2 - Создание ф-ции открытия модального окна
     function openModal() {
         modal.classList.toggle('show');
         document.body.style.overflow = 'hidden';
-        // 3 - Если пользователь сам открыл окно, то не открывать по интервалу
         clearInterval(modalTimeId);
     }
     
@@ -116,14 +109,12 @@ window.addEventListener('DOMContentLoaded', function() {
         btn.addEventListener('click', openModal);
     });
 
-    // Закрытие модального окна
     function closeModal() {
         modal.classList.toggle('show');
         document.body.style.overflow = '';
     }
     modalCloseBtn.addEventListener('click', closeModal);
 
-    // Закрыть после нажатия на кнопку
     modal.addEventListener('click', (e) => {
         if (e.target === modal) {
             closeModal();
@@ -136,17 +127,104 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    // 1 - Создание переменной интервала
     const modalTimeId = setTimeout(openModal, 3000);
 
-    // 4 - Показывать модальное окно при скроле до конца страницы
     function showModalByScroll() {
-        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1 ) { // -1 - для исключения бага
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1 ) {
             openModal();
             window.removeEventListener('scroll', showModalByScroll);
         }
     }
-    // 5 - Обработчик события на окно
+
     window.addEventListener('scroll', showModalByScroll);
+
+
+// Использование классов для карточек
+
+    // 1 - Создание класса (создает только верстку)
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            // 6 - Получение родителя (также можно вычислять в методе'render')
+            this.parent = document.querySelector(parentSelector); // DOM-элемент
+            this.transfer = 74; // курс доллара к рублю
+            this.changeToRUB();
+            
+        }
+
+        // 2 - Конвертация валют (цена в БД будет в долларах)
+        changeToRUB() {
+            // Когда трансформируем число - в него же и записываем рез-т
+            this.price *= this.transfer;
+            // Конвертацию возможно поместить или в метод 'render' или в конструктор
+        }
+
+        // 3 - Формирование верстки
+            // Создать эл-т, в него поместить опред-ную верстку,
+            // верстку дополнить теми данными, которые приходят как аргументы и
+            // поместить э-т на страницу
+        render(){
+            // 4 - Создание э-та
+            const element = document.createElement('div');
+            // 5 - Динамическое формирование э-та
+            element.innerHTML = `
+                <div class="menu__item">
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
+                    </div>
+                </div>
+            `;
+            // 7 - Помещение э-та на страницу
+            this.parent.append(element);
+        }
+    }
+
+    // 8 - Создание нового объекта
+        // Вариант 1:
+            // const div = new MenuCard(аргументы);
+            // div.render();
+    // Вариант 2 - сокращение записи:
+        // Объект может существовать без переменной
+        // Применяется только тогда, когда объект используется только на месте,
+        // без дальнейшего вызова, тк на него нет ссылок
+    
+    // 1
+    new MenuCard(
+        'img/tabs/vegy.jpg',
+        'vegy',
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        '.menu .container'
+    ).render();
+
+    // 2
+    new MenuCard(
+        'img/tabs/elite.jpg',
+        'elite',
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        20,
+        '.menu .container'
+    ).render();
+
+    // 3
+    new MenuCard(
+        'img/tabs/post.jpg',
+        'post',
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        16,
+        '.menu .container'
+    ).render();
 
 });
